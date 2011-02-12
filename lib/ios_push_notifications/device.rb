2963a2d3c789586ebe76f82e76ce8feffd2870d5
Seivan Heidari <seivan@kth.se>
2011-02-12 05:01:26 +0100

    logger.debug params[:device_token].inspect     # res = params[:device_token].scan(/\<(.+)\>/).first     # unless res.nil? || res.empty?     #    params[:device_token] = res.first     # end     #

--- lib/ios_push_notifications/device.rb
+++ lib/ios_push_notifications/device.rb
@@ -4,16 +4,19 @@ module IOSPN
   has_many :notifications
   validates_uniqueness_of :token
   validates_format_of :token, :with => /^[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}\s[a-z0-9]{8}$/
+  
+  def save
+    self.last_registered_at = Time.now if self.last_registered_at.nil?
+    self.token = self.token
+    super()
+  end
+  
   # before_save :set_last_registered_at  
   # private
   # def self.set_last_registered_at
   #   self.last_registered_at = Time.now if self.last_registered_at.nil?
+  #   self.token = self.token
   # end
-  
-  def save
-    self.last_registered_at = Time.now if self.last_registered_at.nil?
-    super()
-  end
   
   def token=(token)
     res = token.scan(/\<(.+)\>/).first
